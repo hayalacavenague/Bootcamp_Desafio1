09b1a9a23fabfabc1155cd6f4c6aabea446e26b4
hayalacavenague <74274626+hayalacavenague@users.noreply.github.com>
2023-04-12 22:04:55 -0300

    Update script_nba.py

--- script_nba.py
+++ script_nba.py
@@ -1,10 +1,14 @@
+# importar pacotes
+
 import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine
 import psycopg2
 
+# lendo dados
 df1 = pd.read_csv("NBA Payroll(1990-2023).csv")
 
+# correcoes nos dados para remover $ e tranformar em int
 df1['payroll'] = df1['payroll'].str.replace('$','')
 df1['payroll'] = df1['payroll'].str.replace(',','')
 df1['payroll'] = df1['payroll'].astype(int)
@@ -13,12 +17,16 @@ df1['inflationAdjPayroll'] = df1['inflationAdjPayroll'].str.replace('$','')
 df1['inflationAdjPayroll'] = df1['inflationAdjPayroll'].str.replace(',','')
 df1['inflationAdjPayroll'] = df1['inflationAdjPayroll'].astype(int)
 
+# criando coluna de data 
 df1['created_at'] = datetime.now()
 
+#removendo coluna desnecessaria
 df1.drop('Unnamed: 0',axis=1,inplace=True)
 
+# criando coluna id
 df1['id'] = df1.index
 
+# criando conexao banco e exportando dados
 conn_string = 'postgresql://root:root@localhost:5432/test_db'
 
 db = create_engine(conn_string)
